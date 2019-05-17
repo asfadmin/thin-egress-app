58f285182d400a92d0683b2b1e9cc3fc3155fc0d
bbuechler <btbuechler@alaska.edu>
2019-05-17 12:58:57 -0800

    [PR-1970] Fix retrieve_secret() error handling. Fix template pathing

--- lambda/common/common.py
+++ lambda/common/common.py
@@ -749,7 +749,7 @@ def get_html_body(template_vars:dict, templatefile:str='root.html'):
         html_template_status = cache_html_templates()
 
     jin_env = Environment(
-        loader=FileSystemLoader([html_template_local_cachedir, os.path.join(os.path.dirname(__file__), '..', "templates")]),
+        loader=FileSystemLoader([html_template_local_cachedir, os.path.join(os.path.dirname(__file__), '../..', "templates")]),
         autoescape=select_autoescape(['html', 'xml'])
     )
     try:
@@ -781,26 +781,8 @@ def retrieve_secret(secret_name):
             SecretId=secret_name
         )
     except ClientError as e:
-        if e.response['Error']['Code'] == 'DecryptionFailureException':
-            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
-            # An error occurred on the server side.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'InvalidParameterException':
-            # You provided an invalid value for a parameter.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'InvalidRequestException':
-            # You provided a parameter value that is not valid for the current state of the resource.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
-            # We can't find the resource that you asked for.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
+        log.error("Encountered fatal error trying to reading URS Secret: {0}".format(e))
+        raise e
     else:
         # Decrypts secret using the associated KMS CMK.
         # Depending on whether the secret is a string or binary, one of these fields will be populated.

8a3e5463ab66216d699bfcc4c7ff497a1893a53d
Ben Barton <benbart@users.noreply.github.com>
2019-08-13 16:46:21 -0800

    PR-2027 making rm happen in ./

--- build/dependency_builder.sh
+++ build/dependency_builder.sh
@@ -11,15 +11,15 @@ cd pkg/python || exit
 pip3 install -r /depbuild/in/requirements.txt --target .
 
 # get rid of unneeded things to make code zip smaller
-rm -rf *.dist-info
-rm -rf pip
-rm -rf docutils
-rm -rf chalice/cli # cli in lambda? No way!
-rm -rf botocore # included with lambda, just takes up space here
-rm -rf setuptools
-rm -rf tests
-rm -rf easy_install.py
-rm -f typing.py # MUST be removed, its presence causes error every time
+rm -rf ./*.dist-info
+rm -rf ./pip
+rm -rf ./docutils
+rm -rf ./chalice/cli # cli in lambda? No way!
+rm -rf ./botocore # included with lambda, just takes up space here
+rm -rf ./setuptools
+rm -rf ./tests
+rm -rf ./easy_install.py
+rm -f ./typing.py # MUST be removed, its presence causes error every time
 
 cd ..
 

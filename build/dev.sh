4a9e88db95deff25fef684be0506b0c317bf4b08
Ben Barton <bbarton@alaska.edu>
2020-03-20 15:38:22 -0800

    PR-2327 dev

--- build/dev.sh
+++ build/dev.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
+echo "starting looking for api gateways"
 
+aws apigateway get-rest-apis --output=text --region ${AWS_DEFAULT_REGION}
 
 API=$(aws apigateway get-rest-apis --query "items[?name=='${STACKNAME}-EgressGateway'].id" --output=text --region ${AWS_DEFAULT_REGION});
 if [ -z $API ];  then exit 1; fi;

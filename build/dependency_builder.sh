7eb15de8a6f6b943714745a53b50b31f6f791519
dpflowersjr <dpflowersjr@alaska.edu>
2020-09-11 14:06:24 -0800

    Added Depbuild changes

--- build/dependency_builder.sh
+++ build/dependency_builder.sh
@@ -8,12 +8,13 @@ yum update -y
 yum install -y zip python3-devel python3-pip
 
 mkdir -p /depbuild/pkg/python
+export depbuild=${DEPBUILD:-"/depbuild/"}
 
-cd /depbuild/pkg/python || exit
+cd /${depbuild}/pkg/python || exit
 
 pip3 install --upgrade setuptools
-pip3 install -r /depbuild/in/requirements_tea.txt --target .
-pip3 install -r /depbuild/in/requirements_rain-api-core.txt --target .
+pip3 install -r /${depbuild}/in/requirements_tea.txt --target .
+pip3 install -r /${depbuild}/in/requirements_rain-api-core.txt --target .
 
 # get rid of unneeded things to make code zip smaller
 rm -rf ./*.dist-info
@@ -28,8 +29,9 @@ rm -f typing.py # MUST be removed, its presence causes error every time
 
 cd ..
 
-echo "zipping to /depbuild/out/${ZIPFILENAME}"
+echo "zipping to /${depbuild}/out/${ZIPFILENAME}"
 
-zip -r9 "/depbuild/out/${ZIPFILENAME}" .
+
+zip -r9 "/${depbuild}/out/${ZIPFILENAME}" .
 
 echo "all done"

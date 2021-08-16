d4397ab6d2f01aafb5b422b57d14dd9bc3d92996
McKade Sorensen <dmsorensen@alaska.edu>
2021-08-16 10:59:22 -0800

    Testing

--- lambda/app.py
+++ lambda/app.py
@@ -556,7 +556,7 @@ def try_download_head(bucket, filename):
 
 
 # Attempt to validate HEAD request
-@app.route('/{proxy+}', methods=['HEAD'])
+@app.route('/{proxy+}', methods=['HEAD'], cors=app.api.cors)
 def dynamic_url_head():
     t = [time.time()]
     log.debug('attempting to HEAD a thing')
@@ -618,7 +618,7 @@ def handle_auth_bearer_header(token):
     return 'return', do_auth_and_return(app.current_request.context)
 
 
-@app.route('/{proxy+}', methods=['GET'])
+@app.route('/{proxy+}', methods=['GET'], cors=app.api.cors)
 def dynamic_url():
     t = [time.time()]
     custom_headers = {}

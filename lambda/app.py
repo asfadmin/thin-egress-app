9d453c62286abed1664d473c73b0431dc39a964f
McKade Sorensen <dmsorensen@alaska.edu>
2021-08-23 13:34:40 -0800

    Removing commit statement

--- lambda/app.py
+++ lambda/app.py
@@ -58,8 +58,7 @@ class TeaChalice(Chalice):
 
 app = TeaChalice(app_name='egress-lambda')
 
-if os.getenv("CORS_ORIGIN"):
-    app.api.cors = True
+app.api.cors = True
 
 
 class TeaException(Exception):

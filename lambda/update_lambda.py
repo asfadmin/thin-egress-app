ae057ec0584d985dadcad29883b6121867b25879
Brian Buechler <btbuechler@alaska.edu>
2021-10-18 13:32:44 -0800

    Update update_lambda.py

--- lambda/update_lambda.py
+++ lambda/update_lambda.py
@@ -105,8 +105,8 @@ def get_base_policy(prefix):
                 "s3:GetBucketLocation"
             ],
             "Resource": [
-                "arn:aws:s3:::asf-ngap2w-p-*/*",
-                "arn:aws:s3:::asf-ngap2w-p-*"
+                """ + f'"arn:aws:s3:::{prefix}' + """*/*",
+                """ + f'"arn:aws:s3:::{prefix}' + """*"
             ],
             "Effect": "Allow",
             "Condition": {

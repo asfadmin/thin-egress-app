e144d3a3f3597c8a431e054eb567995ae88f9e6a
Rohan Weeden <reweeden@alaska.edu>
2022-01-19 08:14:55 -0900

    Move test resource helpers to conftest

--- tests/__init__.py
+++
@@ -1,8 +0,0 @@
-import pathlib
-from typing import IO, Any
-
-RESOURCES_PATH = pathlib.Path(__file__).parent.joinpath("resources/").absolute()
-
-
-def open_resource(file: str, *args, **kwargs) -> IO[Any]:
-    return (RESOURCES_PATH / file).open(*args, **kwargs)
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,17 @@
 import importlib
 import logging
+import pathlib
 import sys
+from typing import IO, Any
 from unittest import mock
 
 import pytest
 
-from tests import open_resource
-
 # Can't import normally because 'lambda' is a reserved word
 # Need to import the app module first because it will override the log level at import time
 _ = importlib.import_module("lambda.app")
+
+RESOURCES_PATH = pathlib.Path(__file__).parent.joinpath("resources/").absolute()
 
 logging.getLogger().setLevel(0)
 
@@ -20,11 +22,10 @@ mock_boto3 = mock.create_autospec(real_boto3)
 sys.modules["boto3"] = mock_boto3
 
 
-# TODO(reweeden): Use an existing pytest plugin
 class Resources():
     """Helper for getting test resources"""
-    def open(self, *args, **kwargs):
-        return open_resource(*args, **kwargs)
+    def open(self, file, *args, **kwargs) -> IO[Any]:
+        return (RESOURCES_PATH / file).open(*args, **kwargs)
 
 
 @pytest.fixture(scope="session")

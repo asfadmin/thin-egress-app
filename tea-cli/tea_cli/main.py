fa745f3a2af765d4b765f892caa08c430362a84e
Rohan Weeden <reweeden@alaska.edu>
2023-07-10 12:02:46 -0400

    Handle KeyboardInterrupts

--- tea-cli/tea_cli/main.py
+++ tea-cli/tea_cli/main.py
@@ -4,13 +4,19 @@ import sys
 
 from tea_cli.commands import list_versions, quick_deploy
 
+log = logging.getLogger(__name__)
+
 
 def main(args=None):
     parser = get_parser()
     parsed_args = parser.parse_args(args=args)
 
     setup_logger(parsed_args)
-    parsed_args.func(parsed_args)
+
+    try:
+        parsed_args.func(parsed_args)
+    except KeyboardInterrupt:
+        log.info("Cancelled")
 
 
 def get_parser() -> argparse.ArgumentParser:
